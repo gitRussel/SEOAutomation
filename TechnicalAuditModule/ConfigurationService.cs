6cbac14553f5354f90cb45c5bcdc9a703fa8e884
Ruslan Tinkov <rtinkov@deloitte.com>
2020-05-11 00:56:12 +0300

    Начало сервсиа технического аудита

---
+++ TechnicalAuditModule/ConfigurationService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechnicalAuditModule
+{
+    public class ConfigurationService
+    {
+        private IConfiguration _config;
+
+        public ConfigurationService(IConfiguration config)
+        {
+            _config = config;
+            string key = _config["PageSpeedInsights:ServiceApiKey"]
+        }
+
+    }
+}
